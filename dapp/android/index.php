8ec88de72da7d310996674e544b14c1c3eb73930
JB9623 <gt20181122@gmail.com>
2023-04-14 07:13:34 -0400

    init upload

---
+++ dapp/android/index.php
@@ -0,0 +1,7 @@
+<?php
+/* Redirect browser */
+header("Location: https://bitsport.io/android/BitSport_V1.0.apk");
+ 
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
